b38d2d92ba1ec21f80e6d28fc8382e5088f044bc
Aleksey02 <lyosha2002stich@gmail.com>
2024-08-12 21:25:18 +0500

    feat: Add adaptive to header, home, footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import logo from '../../assets/images/Header/logo.svg'
+import miniLogo from '../../assets/images/Header/mini_logo.svg'
 import classes from './Header.module.scss'
 
 function Header(){
+    const [isBurgerActive, setIsBurgerActive] = useState(false);
     return (
         <header className={classes.header}>
             <a href="/" className={classes.header__logo}>
-                <img src={logo} alt="logo" />
+                <img src={logo} alt="logo" className={classes.header__logo + ' ' + classes.header__logo_long}/>
+                <img src={miniLogo} alt="logo" className={classes.header__logo + ' ' + classes.header__logo_mini}/>
             </a>
             <ul className={classes.header__list}>
                 <li className={classes.header__item}>
@@ -19,6 +23,16 @@ function Header(){
                 </li>
             </ul>
             <button className={classes.header__btn}>Войти</button>
+            <button className={classes.burger + ' ' + (isBurgerActive ? classes.active : '')} onClick={()=>setIsBurgerActive(prev=>!prev)}>
+                <span className={classes.burger__line}></span>
+                <span className={classes.burger__line}></span>
+                <span className={classes.burger__line}></span>
+            </button>
+            <div className={classes.menu + ' ' + (isBurgerActive ? classes.active : '')}>
+                <a href="/" className={classes.menu__link}>ЕГЭ</a>
+                <a href="/" className={classes.menu__link}>ОГЭ</a>
+                <a href="/" className={classes.menu__link}>Помощь</a>
+            </div>
         </header>
     )
 }
