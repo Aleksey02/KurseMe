fbc6c164a127a078490aa4cc4d3d50814eb52fd9
Aleksey02 <lyosha2002stich@gmail.com>
2024-08-14 13:06:51 +0500

    feat: Add link to socials networks

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,13 +18,13 @@ function Footer(){
             </div>
             <div className={classes.footer__item}>
                 <h4 className={classes.footer__title}>О нас</h4>
-                <a className={classes.footer__text}>Поддержка</a>
-                <a className={classes.footer__text}>Отзывы</a>
+                <a href='https://t.me/kursemesup' target='_blank' className={classes.footer__text}>Поддержка</a>
+                <a href='https://t.me/+G2DF-9J14ikzMTJi' target='_blank' className={classes.footer__text}>Отзывы</a>
             </div>
             <div className={classes.footer__item}>
                 <h4 className={classes.footer__title}>Связаться с нами</h4>
-                <a className={classes.footer__text}>Наш Telegram канал</a>
-                <a className={classes.footer__text}>Менеджер</a>
+                <a href='https://t.me/+TibCIQYxcN1mMDFl' className={classes.footer__text}>Наш Telegram канал</a>
+                <a href='https://t.me/kursemesup' className={classes.footer__text}>Менеджер</a>
             </div>
         </footer>
     )
