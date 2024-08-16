67a23da4d989657bf00fd61f84b2d14ee0099e77
Aleksey02 <lyosha2002stich@gmail.com>
2024-08-16 23:21:17 +0500

    feat: Add id to faq block

--- src/components/Main/Home/FAQ/FAQ.jsx
+++ src/components/Main/Home/FAQ/FAQ.jsx
@@ -4,7 +4,7 @@ import classes from './FAQ.module.scss'
 function FAQ({data}){
     
     return (
-        <div className={classes.faq}>
+        <div className={classes.faq} id="faq">
             <h2 className={classes.faq__title}>FAQ</h2>
             <div className={classes.faq__box}>
                 {data.map((item, index)=><Item info={item} key={index}/>)}
