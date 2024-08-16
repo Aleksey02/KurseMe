1bffa0104efcd64076566e5f82d66ff60bdc4c11
Aleksey02 <lyosha2002stich@gmail.com>
2024-08-16 11:21:45 +0500

    feat: Add header fixed and add button in FAQ

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,30 +7,32 @@ function Header(){
     const [isBurgerActive, setIsBurgerActive] = useState(false);
     return (
         <header className={classes.header}>
-            <NavLink to="/" className={classes.header__logo}>
-                <img src={logo} alt="logo" className={classes.header__logo}/>
-            </NavLink>
-            <ul className={classes.header__list}>
-                <li className={classes.header__item}>
-                    <NavLink to="/11" className={classes.header__link}>ЕГЭ</NavLink>
-                </li>
-                <li className={classes.header__item}>
-                    <NavLink to="/9" className={classes.header__link}>ОГЭ</NavLink>
-                </li>
-                <li className={classes.header__item}>
-                    <NavLink href="/" className={classes.header__link}>Бесплатные Курсы</NavLink>
-                </li>
-            </ul>
-            <button className={classes.header__btn}>Войти</button>
-            <button className={classes.burger + ' ' + (isBurgerActive ? classes.active : '')} onClick={()=>setIsBurgerActive(prev=>!prev)}>
-                <span className={classes.burger__line}></span>
-                <span className={classes.burger__line}></span>
-                <span className={classes.burger__line}></span>
-            </button>
-            <div className={classes.menu + ' ' + (isBurgerActive ? classes.active : '')}>
-                <NavLink to="/11" className={classes.menu__link}>ЕГЭ</NavLink>
-                <NavLink to="/9" className={classes.menu__link}>ОГЭ</NavLink>
-                <NavLink to="/" className={classes.menu__link}>Бесплатные Курсы</NavLink>
+            <div className={classes.header__box}>
+                <NavLink to="/" className={classes.header__logo}>
+                    <img src={logo} alt="logo" className={classes.header__logo}/>
+                </NavLink>
+                <ul className={classes.header__list}>
+                    <li className={classes.header__item}>
+                        <NavLink to="/11" className={classes.header__link}>ЕГЭ</NavLink>
+                    </li>
+                    <li className={classes.header__item}>
+                        <NavLink to="/9" className={classes.header__link}>ОГЭ</NavLink>
+                    </li>
+                    <li className={classes.header__item}>
+                        <NavLink href="/" className={classes.header__link}>Бесплатные Курсы</NavLink>
+                    </li>
+                </ul>
+                <button className={classes.header__btn}>Войти</button>
+                <button className={classes.burger + ' ' + (isBurgerActive ? classes.active : '')} onClick={()=>setIsBurgerActive(prev=>!prev)}>
+                    <span className={classes.burger__line}></span>
+                    <span className={classes.burger__line}></span>
+                    <span className={classes.burger__line}></span>
+                </button>
+                <div className={classes.menu + ' ' + (isBurgerActive ? classes.active : '')}>
+                    <NavLink to="/11" className={classes.menu__link}>ЕГЭ</NavLink>
+                    <NavLink to="/9" className={classes.menu__link}>ОГЭ</NavLink>
+                    <NavLink to="/" className={classes.menu__link}>Бесплатные Курсы</NavLink>
+                </div>
             </div>
         </header>
     )
--- src/components/Main/Home/FAQ/FAQ.jsx
+++ src/components/Main/Home/FAQ/FAQ.jsx
@@ -9,6 +9,7 @@ function FAQ({data}){
             <div className={classes.faq__box}>
                 {data.map((item, index)=><Item info={item} key={index}/>)}
             </div>
+            <a href="https://t.me/egeball12_bot" target='_blank' className={classes.faq__link}>КУПИТЬ КУРСЫ</a>
         </div>
     )
 }
