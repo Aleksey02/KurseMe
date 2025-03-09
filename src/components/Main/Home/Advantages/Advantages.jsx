5ebdc89e6d139af0d717f6dc54e63603541a660a
Aleksey02 <lyosha2002stich@gmail.com>
2025-03-09 14:56:27 +0500

    Удалил подмодуль backend

--- src/components/Main/Home/Advantages/Advantages.jsx
+++ src/components/Main/Home/Advantages/Advantages.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import classes from './Advantages.module.scss'
 import Point from './Point/Point'
 
@@ -13,4 +14,21 @@ function Advantages({data}){
     )
 }
 
+=======
+import classes from './Advantages.module.scss'
+import Point from './Point/Point'
+
+function Advantages({data}){
+    
+    return (
+        <div className={classes.advantages}>
+            <h2 className={classes.advantages__title}>Преимущества</h2>
+            <div className={classes.advantages__box}>
+                {data.map((item, index)=><Point info={item} key={index}/>)}
+            </div>
+        </div>
+    )
+}
+
+>>>>>>> e7598d7 (Удалил подмодуль backend)
 export default Advantages
