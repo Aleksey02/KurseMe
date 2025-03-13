25efdfdecd8f9bb0fcee710431082646e9fc0ca1
Aleksey02 <lyosha2002stich@gmail.com>
2025-03-13 09:07:51 +0500

    fix: Fix CourseAdvertising block

--- src/components/Main/Home/CourseAdvertising/CourseAdvertising.jsx
+++ src/components/Main/Home/CourseAdvertising/CourseAdvertising.jsx
@@ -1,4 +1,5 @@
 import classes from './CourseAdvertising.module.scss'
+import advertImg from '../../../../assets/images/Home/CourseAdvertising/advert_image.png'
 
 function CourseAdvertising() {
 	return <div className={classes.courseAdvertising}>
@@ -7,11 +8,11 @@ function CourseAdvertising() {
 				<p className={classes.courseAdvertising__label}>egeball20_bot</p>
 				<h2 className={classes.courseAdvertising__title}>Сливы курсов ЕГЭ и ОГЭ в 10 раз дешевле.</h2>
 				<p className={classes.courseAdvertising__desc}>С 2020 года мы помогли 10,000+ выпускникам подготовиться к экзаменам почти за бесценок! Присоединяйся к нашей пиратской семье и занимайся по лучшим курсам за копейки. ❤️</p>
+				<div className={classes.courseAdvertising__image}>
+					<img src={advertImg} alt="Сливы курсов ЕГЭ и ОГЭ в 10 раз дешевле." />
+				</div>
 				<a href='https://t.me/egeball20_bot' target='_blank' className={classes.courseAdvertising__btn}>Купить Курсы</a>
 			</div>
-			{/* <div className={classes.courseAdvertising__image}>
-				<img src={advertImg} alt="Сливы курсов ЕГЭ и ОГЭ в 10 раз дешевле." />
-			</div> */}
 		</div>
 	</div>
 }
