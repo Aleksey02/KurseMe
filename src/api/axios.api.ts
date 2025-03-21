d50a86af39feeec063cbb317ab86d6fc04355443
Aleksey02 <lyosha2002stich@gmail.com>
2025-03-21 13:16:25 +0500

    fix: fix docker-compose

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getTokenFromLocalStorage } from '../helper/localstorage.helper';
 
 export const instance = axios.create({
-	baseURL: 'http://localhost:3000/api',
+	baseURL: 'http://egeball-backendcompose-3bd6ad-324ba3-109-107-157-189.traefik.me/api',
 	headers: {
 		Authorization: 'Bearer ' + getTokenFromLocalStorage(),
 	}
