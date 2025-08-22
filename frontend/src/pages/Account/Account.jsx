1ac1213a87dd33d643d8c20c79ac57666e24313a
Aleksey02 <lyosha2002stich@gmail.com>
2025-08-22 20:22:33 +0500

    fix: Fix SetCookie

--- frontend/src/pages/Account/Account.jsx
+++ frontend/src/pages/Account/Account.jsx
@@ -10,18 +10,17 @@ function Account({isAuth, setIsAuth}) {
 	const navigate = useNavigate();
 
 	useEffect(() => {
-		console.log(document.cookie, 'cookie');
-		// setTimeout(()=>{
-		// 	const response = axios.get('https://egeball.com/api/api/auth/loginToBot', {withCredentials: true})
-		// 		.then(res => {
-		// 			console.log(res);
-		// 			setIsAuth(res.data)
-		// 		})
-		// 		.catch(error => {
-		// 		console.log('Ошибка получения пользователя', error);
-		// 		// Пользователь не авторизован, или ошибка сети
-		// 		});
-		// }, 1000)
+		setTimeout(()=>{
+			const response = axios.get('https://egeball.com/api/api/auth/loginToBot', {withCredentials: true})
+				.then(res => {
+					console.log(res);
+					setIsAuth(res.data)
+				})
+				.catch(error => {
+				console.log('Ошибка получения пользователя', error);
+				// Пользователь не авторизован, или ошибка сети
+				});
+		}, 1000)
 		
 	}, [])
 
