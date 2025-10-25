fadcb24b73d1957ddbc1c1e05faaacd402d82918
Aleksey02 <lyosha2002stich@gmail.com>
2025-10-25 14:48:32 +0500

    feat: Add redirect to egeball.live for russian users

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,18 @@ import data from './data/data.js'
 import { ToastContainer } from 'react-toastify'
 
 
+fetch('https://ipinfo.io/json?token=token')
+  .then(res => res.json())
+  .then(data => {
+    if (data.country === 'RU') {
+      window.location.href = 'https://egeball.live' + window.location.pathname;
+    } else {
+      createRoot(document.getElementById('root')).render(
+        <StrictMode>
+          <App data={data} />
+          <ToastContainer position='bottom-right' autoClose={2000} />
+        </StrictMode>,
+      )
+    }
+  });
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App data={data} />
-    <ToastContainer position='bottom-right' autoClose={2000} />
-  </StrictMode>,
-)
