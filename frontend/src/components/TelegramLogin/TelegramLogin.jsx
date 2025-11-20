0e5485583a5205b4009ef55ecd0bfdacfd730488
Aleksey02 <lyosha2002stich@gmail.com>
2025-11-20 22:22:11 +0500

    fix: Fix bug with login

--- frontend/src/components/TelegramLogin/TelegramLogin.jsx
+++ frontend/src/components/TelegramLogin/TelegramLogin.jsx
@@ -7,7 +7,7 @@ const TelegramLogin = ({setIsAuth}) => {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
     script.async = true;
-    script.setAttribute('data-telegram-login', 'egeball21_bot');
+    script.setAttribute('data-telegram-login', 'egeball22_bot');
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-auth-url', 'https://snosy.cc/v1/api/login/');
     script.setAttribute('data-request-access', 'write');
