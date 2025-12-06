10cb91d811b3cbed9393766474f9fc8ea61f6f31
Aleksey02 <lyosha2002stich@gmail.com>
2025-12-06 19:23:51 +0500

    fix: Fix link to tg auth

--- frontend/src/components/TelegramLogin/TelegramLogin.jsx
+++ frontend/src/components/TelegramLogin/TelegramLogin.jsx
@@ -9,7 +9,7 @@ const TelegramLogin = observer(({setIsAuth}) => {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?22';
     script.async = true;
-    script.setAttribute('data-telegram-login', botLinkStore.link);
+    script.setAttribute('data-telegram-login', botLinkStore.authLink);
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-auth-url', 'https://snosy.cc/v1/api/login/');
     script.setAttribute('data-request-access', 'write');
