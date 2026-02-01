8d7360da26eac3490b672c417a95a93c3775a194
Aleksey02 <lyosha2002stich@gmail.com>
2026-02-01 23:09:22 +0500

    fix: Change link to ai telegram bot

--- frontend/src/pages/RedirectAI/RedirectAI.jsx
+++ frontend/src/pages/RedirectAI/RedirectAI.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function RedirectAI() {
 	useEffect(() => {
-		window.location.href = "https://t.me/egeballAI_bot";
+		window.location.href = "https://t.me/agent4ege_bot";
 	}, []);
 
 	return (
